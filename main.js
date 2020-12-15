ed44271efb45510f589a2fa14126366a449f91c1
kawaitoi <kawaitoi123@gail.com>
2020-12-16 03:11:59 +0900

    feat(html):aside追加,ロゴ追加

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const parentMenu = document.querySelectorAll(".menu > li > a");
+const parentMenu = document.querySelectorAll(" .menu > li > a");
 for(let i = 0; i < parentMenu.length; i++){
    parentMenu[i].addEventListener("click", function(e){
        e.preventDefault();
